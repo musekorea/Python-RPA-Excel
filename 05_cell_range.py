e85ae900fea1aa22647362bf4f358ae3a7d39443
musekorea <musekorea@hanmail.net>
2022-03-14 15:05:16 +0800

    cell range

--- 05_cell_range.py
+++ 05_cell_range.py
@@ -9,15 +9,11 @@ ws.append(["#", "English","Math"])
 for i in range(1,11):
   ws.append([i, random.randint(0,100),random.randint(0,100)])
 
-scores = ws[2:ws.max_row]
-for score in scores:
-  for cell in score:
-    #print(cell.value, end=" ")
-    #print(cell.coordinate, end=" ")
-    xy = coordinate_from_string(cell.coordinate)
-    print("column=", xy[0])
-    
-  print()
+for row in ws.iter_rows(min_row=1, max_row=5, min_col=1, max_col=1):
+  print(row)
+  
 
-wb.save("test.xlsx")
+
+
+#wb.save("test.xlsx")
 wb.close
