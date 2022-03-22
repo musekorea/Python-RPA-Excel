10a7a2844454aa2b2ac8d95dc185341109982e6e
musekorea <musekorea@hanmail.net>
2022-03-22 23:13:53 +0800

    style

--- 11_cell_style.py
+++ 11_cell_style.py
@@ -1,24 +1,19 @@
 from openpyxl import load_workbook
-from openpyxl.styles import Font, Border, Side
+from openpyxl.styles import Font, Border, Side, PatternFill, Alignment
 
 wb = load_workbook("./test.xlsx")
 ws = wb.active
 
-a1 = ws["A1"]
-b1 = ws["B1"]
-c1 = ws["C1"]
+for row in ws.rows:
+  for cell in row: 
+    cell.alignment = Alignment(horizontal="center", vertical="center")
+    if cell.column==1:
+      continue
+    if isinstance(cell.value, int) and cell.value>90:
+      cell.fill = PatternFill(fgColor="00FF00", fill_type="solid")
+      cell.font = Font(color="FF0000")
 
-ws.column_dimensions["A"].width = 5
-ws.row_dimensions[1].height = 50
-
-a1.font = Font(color="FF0000", italic=True, bold=True)
-b1.font = Font(color="CC33FF", name="Arial", strike=True)
-c1.font = Font(color="0000FF", size=20, underline="double")
-
-title_border = Border(left=Side(style="thin"), right=Side(style="thin"), top=Side(style="thin"), bottom=Side(style="thin"))
-a1.border = title_border
-b1.border = title_border
-c1.border = title_border
+ws.freeze_panes = "B2"
 
 wb.save("new.xlsx")
 wb.close()
